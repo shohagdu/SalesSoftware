4f49c975142e6339ae448e953ac944c3e1f12e7b
omarshohagdu <omarfaruk@du.ac.bd>
2021-10-30 02:11:42 +0600

    update user access list

--- app/views/dashboard/userAccess/userAccessList.php
+++ app/views/dashboard/userAccess/userAccessList.php
@@ -1,63 +1,67 @@
-
-<!-- Main content -->
 <section class="content">
-    <div class="box">
-        <div class="box-header">
-            <h3 class="box-title">User Info</h3>
-            <?php if ($this->session->flashdata('msg')) { ?>
-                <?php echo $this->session->flashdata('msg'); ?>
-            <?php } ?>
-            <a href="<?php echo site_url('settings/addUser'); ?>"
-               class="btn btn-primary btn-sm pull-right"><i class="glyphicon glyphicon-plus"></i> Add</a>
-        </div>
-        <!-- /.box-header -->
-        <div class="box-body">
-            <table id="example1" class="table table-bordered table-hover">
-                <thead>
-                <tr>
-                    <th>S/L</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Type</th>
-                    <th>Status</th>
-                    <th>Action</th>
-                </tr>
-                </thead>
-                <tbody>
-                <?php
-                $i=1;
-                if(!empty($admin_list)){
-                    foreach ($admin_list as $each_admin) {
-                        ?>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="box">
+                <div class="box-body" id="alert" style="display: none;"> <div class="callout callout-info"><span
+                                id="show_message"></span></div></div>
+                <div class="box-header">
+                    <h3 class="box-title pull-left"><?php echo !empty($title)?$title:'' ?></h3>
+                    <?php if ($this->session->flashdata('msg')) { ?>
+
+                        <?php echo $this->session->flashdata('msg'); ?>
+
+                    <?php } ?>
+                    <h3 class="box-title pull-right" style="padding-right:10px;"><a class="btn btn-success btn-sm"   href="<?php echo site_url('UserAccessRole/userAccessCreate') ?>"><i class="glyphicon glyphicon-plus"></i> Add</a></h3>
+                </div>
+                <div class="box-body">
+                    <table id="example1" class="table table-bordered table-striped">
+                        <thead>
                         <tr>
-                            <td><?php echo $i++; ?></td>
-                            <td><?php echo $each_admin->username; ?></td>
-                            <td><?php echo $each_admin->email; ?></td>
-                            <td>
-                                <?php echo $each_admin->role_name  ?>
-                            </td>
-                            <td>
-                                <span class="badge <?php echo (!empty($each_admin->status) && $each_admin->status=='Active')?'bg-green-active':'bg-red-active' ?>  "> <?php echo $each_admin->status  ?></span>
-                            </td>
-
-
-                            <td>
-                                <a style="margin-right: 5px;" href="<?php echo base_url('settings/editadmin'); ?>/<?php echo $each_admin->userID; ?>" class="btn btn-primary btn-sm pull-left"><i class="glyphicon glyphicon-pencil"></i> Update</a>
-                            </td>
+                            <th class="width10per">SL</th>
+                            <th>Role Name </th>
+                            <th>Status </th>
+                            <th class="width20per"></th>
                         </tr>
-
+                        </thead>
+                        <tbody>
                         <?php
-                    }
-                }
-                ?>
-                </tbody>
-                <tfoot>
+                        $si = 1;
+                        if(!empty($all_acl_menu_info)) {
+                            foreach ($all_acl_menu_info as $row) {
+                                ?>
+                                <tr>
+                                    <td class="vertical-align-middle"><?php echo $si; ?></td>
+                                    <td class="vertical-align-middle"><?php echo $row->role_name; ?></td>
+                                    <td class="vertical-align-middle"><?php echo (!empty($row->is_active) && $row->is_active==1)?"Active":"InActive" ; ?></td>
 
-                </tfoot>
-            </table>
+                                    <td class="vertical-align-middle text-center">
+
+                                        <a class="btn btn-info btn-xs" href="<?php echo site_url('UserAccessRole/userAccessEdit/'.$row->id) ?>"><i class="glyphicon
+                                    glyphicon-pencil"></i> Edit</a>
+                                    </td>
+                                </tr>
+                                <?php
+                                $si++;
+                            }
+                        }
+                        ?>
+                        </tbody>
+                        <tfoot>
+
+                        </tfoot>
+                    </table>
+                </div>
+            </div>
         </div>
-        <!-- /.box-body -->
+
     </div>
-    <!-- /.box -->
 </section>
-<!-- /.content -->
+
+
+
+
+
+
+
+
+
